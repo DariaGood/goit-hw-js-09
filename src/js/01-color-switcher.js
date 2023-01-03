9b73e199d3fc1e1974d0e5d08a4d8812ab112b2e
DariaGood <darina.goodzovska@gmail.com>
2023-01-04 00:32:44 +0100

    2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const refs = {
+    startBtn: document.querySelector('button[data-start]'),
+    stopBtn: document.querySelector('button[data-stop]'),   
+ };
+ 
+ let timerId = null;
+ 
+ function getRandomHexColor() {
+     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+   };
+ 
+ function updateBodyBGcolor(color) {
+   document.body.style.backgroundColor = color;
+ }
+ 
+ refs.startBtn.addEventListener('click', () => {
+   document.querySelector('button[data-start]').disabled = true;
+   document.querySelector('button[data-stop]').disabled = false;
+   timerId = setInterval(() => updateBodyBGcolor(getRandomHexColor()),1000);
+ });
+ 
+ refs.stopBtn.addEventListener('click', () => {
+   document.querySelector('button[data-start]').disabled = false;
+   document.querySelector('button[data-stop]').disabled = true;
+   clearInterval(timerId);
+ });
+ 
+ 
+ 
 
