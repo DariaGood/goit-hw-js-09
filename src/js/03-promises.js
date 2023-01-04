337cc76abb241e05c2e0e9744721301e106d80b8
DariaGood <darina.goodzovska@gmail.com>
2023-01-04 01:04:19 +0100

    4

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,46 @@
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import Notiflix from 'notiflix';
+import 'notiflix/dist/notiflix-3.2.5.min.css';
 
-const refs = {
-  form: document.querySelector('.form'),
-};
+
+// Form and its inputs
+
+const form = document.querySelector('.form');
+
+const firstDelayMs = document.querySelector('[name="delay"]');
+const delayStepMs = document.querySelector('[name="step"]');
+const amount = document.querySelector('[name="amount"]');
+
+// Event listener after submitting the form
+
+form.addEventListener('submit', submitCreatePromises);
+
+// Loop to create promises from function createPromise after event listener
+
+function submitCreatePromises(e) {
+
+  e.preventDefault();
+
+  let delay = firstDelayMs.valueAsNumber;
+  const delayStepMsVal = delayStepMs.valueAsNumber;
+  const amountVal = amount.valueAsNumber;
+
+  for (let i = 1; i <= amountVal; i++) {
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${i} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${i} in ${delay}ms`
+        );
+      });
+    delay += delayStepMsVal;
+  }
+}
+
+//function createPromise
 
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
@@ -16,31 +54,3 @@ function createPromise(position, delay) {
     }, delay);
   });
 }
-
-function onCreatePromises(e) {
-  e.preventDefault();
-  const formData = new FormData(e.currentTarget);
-  const dataParams = {};
-
-  for (const [key, value] of formData.entries()) {
-    dataParams[key] = Number(value);
-  }
-
-  let { amount, step, delay } = dataParams;
-
-  for (let i = 1; i <= amount; i += 1) {
-    delay += step;
-    createPromise(i, delay).then(onSuccess).catch(onError);
-
-    refs.form.reset();
-  }
-}
-
-function onError({ position, delay }) {
-  Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-}
-
-function onSuccess({ position, delay }) {
-  Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-}
-
